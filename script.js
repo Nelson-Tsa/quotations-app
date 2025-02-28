bb926845819fc640adc1c8141db30c559dee904c
Nelson Tsa <t.nelson@hotmail.fr>
2025-02-28 14:26:16 +0100

    suppression des fonctions non utilisés et méthodes inutiles

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let auteurValue = '';
 let citationValue = '';
 let quoteList = [];
 let quoteCount = 0;
+const boutton = document.querySelector('#delete');
 
 //recuperer le contenu des valeurs auteur et citation
 form.addEventListener('submit', (e) => {
@@ -18,7 +19,6 @@ form.addEventListener('submit', (e) => {
     //ajouter une citation
     function addQuote(auteur, citation) {
         const quote = document.createElement('div');
-        quote.classList.add('quote');
         quote.innerHTML = `
         <p class="text">"${citation}"</p>
         <p class="author">${auteur}</p>
@@ -26,52 +26,35 @@ form.addEventListener('submit', (e) => {
         document.querySelector('#quote-list').appendChild(quote);
         quoteCount += 1;
         document.querySelector('#nbCitations').textContent = quoteCount;
-        localStorageSave(auteur, citation);
-        
+        localStorageSave(auteur, citation); 
     }
 
     //sauvegarder dans le local storage
     function localStorageSave(auteur, citation) {
-   
         quoteList.push({auteur, citation});
         localStorage.setItem('quoteList', JSON.stringify(quoteList));
         console.log(quoteList);
     }
 
-//ajouter les citations sauvegardées dans le quotelist
-    function addSavedQuotes() {
-        const savedQuotes = localStorage.getItem('quoteList');
-        if (savedQuotes) {
-            quoteList = JSON.parse(savedQuotes);
-            quoteCount = 0;
-        }
-        quoteList.forEach(quote => {
-            addQuote(quote.auteur, quote.citation);
-        });
-    }
-
-    //addSavedQuotes();
 
     function clearLocalStorage() {
         localStorage.clear();
         location.reload();
     }
     
-    boutton = document.querySelector('#delete');
+    
     boutton.addEventListener('click', clearLocalStorage);
     
-   //clearLocalStorage();
 
    // charger les citations sauvegardées au demarrage
    function loadStorage() {
-    loadStorage = localStorage.getItem('quoteList');
-    if (loadStorage) {
-        quoteList = JSON.parse(loadStorage);
-        quoteCount = 0;
+   const donneesSauvegarde = localStorage.getItem('quoteList');
+    if (donneesSauvegarde) {
+        quoteList = JSON.parse(donneesSauvegarde);
     }
     quoteList.forEach(quote => {
         document.querySelector('#quote-list').innerHTML += `
-        <div class="quote">
+        <div>
         <p class="text">"${quote.citation}"</p>
         <p class="author">${quote.auteur}</p>
         </div>
@@ -79,4 +62,5 @@ form.addEventListener('submit', (e) => {
         document.querySelector('#nbCitations').textContent = quoteList.length;
     });
 }
+
 loadStorage();
